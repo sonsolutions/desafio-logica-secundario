335f6dc9b607e6dc28a1c9b71c4823c9305d03ff
sonsolutions <son.rogerio@outlook.com>
2025-03-09 16:38:05 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+//Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
+//depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+
+
+
+function rackeadas(vitorias, derrotas) {
+   return saldo = vitorias - derrotas;
+  }
+
+rackeadas(60,5)
+
+
+// Estrutura de Decisão
+if(saldo <10)
+{
+    nivel = "Ferro"
+} else if (saldo >= 11 && saldo <= 20) 
+    {
+    nivel = "Bronze";
+} else if (saldo >= 21 && saldo <= 50) 
+    {
+    nivel = "Prata";
+} else if (saldo >= 51 && saldo <= 80) 
+    {
+    nivel = "Ouro";
+} else if (saldo >= 81 && saldo <= 90) 
+    {
+    nivel = "Diamante";
+} else if (saldo >= 91 && saldo <= 100) 
+    {
+    nivel = "Lendário";
+} else {
+    nivel = "Imortal";
+}
+
+//Output
+console.log ("O Herói tem o saldo de " + saldo + " está no nível de " + nivel)
+
